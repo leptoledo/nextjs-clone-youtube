e7d1a3ae017793415489e75570b98bdebfcf9942
Leandro de Paula Toledo <leandrotoledo@hotmail.com.br>
2021-02-26 09:48:42 +0000

    Material-UI configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
 export default function Home() {
   return (
     <div>
       Clone youtube com Next.js + Material UI
+      <Button variant="outlined" color="secondary">
+        Hello Leandro Toledo
+      </Button>
     </div>
   )
 }
